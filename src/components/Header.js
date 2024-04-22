52bb1330b633c1e1d8232d551a255324deae77a2
Herve Kouam <kouamherve01@gmail.com>
2024-04-22 18:11:11 -0400

    📦  F01: APP > Header > Add React-router dom package

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <nav className=" bg-black text-white flex items-center justify-between p-3">
-      <h2 className=" border border-green-600 w-fit">herve kouam</h2>
+      <h2 className=" border border-green-600 w-fit uppercase font-medium tracking-wider">
+        herve kenmegne
+      </h2>
       <span className=" border border-yellow-600">menu-icon</span>
     </nav>
   );
