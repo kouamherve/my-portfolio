2ff42635bf479cbbb68b8db02caa859bc4c9f6a9
Herve Kouam <kouamherve01@gmail.com>
2024-04-22 18:04:29 -0400

    ✨ F01: APP > Header > Add layout

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav className=" bg-black text-white flex items-center justify-between p-3">
+      <h2 className=" border border-green-600 w-fit">herve kouam</h2>
+      <span className=" border border-yellow-600">menu-icon</span>
+    </nav>
+  );
+}
