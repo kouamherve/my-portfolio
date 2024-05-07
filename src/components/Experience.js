b629a0552dc62863bf3d0beb803c87171adf2ca8
Herve Kouam <kouamherve01@gmail.com>
2024-05-07 19:59:04 -0400

    🚧  F03: APP > Experience > Add MD breakpoint

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,39 +7,41 @@ export default function Experience() {
     <section id="experience" className="">
       <div className="font-Poppins text-center">
         <div className="p-8">
-          <h1 className="text-2xl uppercase font-normal mt-8 pb-2">
+          <h1 className="uppercase tracking-wider text-2xl md:text-3xl text-black mt-16 pb-2">
             Experience
           </h1>
-          <p className="text-gray-600 text-center text-base font-normal leading-7 mb-2 p-6">
+          <p className="text-gray-600 text-center text-base leading-7 mb-4 p-6 md:px-10 md:py-6">
             I've been doing web development for about 3 years now, and I'm
             always eager to learn more in this fast paced industry.
           </p>
-          <hr className=" border-2 border-gray-300 max-w-[100px] mx-auto" />
+          <hr className=" border-2 border-gray-300 max-w-[100px] mx-auto md:max-w-[300px] md:mb-2" />
         </div>
-        <div>
-          <h2 className=" text-lg uppercase mt-5 mb-10 mx-4 font-medium">
-            Some technologies I've worked with:
-          </h2>
-          <div className=" text-gray-500 text-5xl flex flex-wrap gap-10 items-center justify-center mb-16 mx-2 px-5">
-            <CardTech iconName="html" title="HTML" />
-            <CardTech iconName="css" title="CSS" />
-            <CardTech iconName="tailwindcss" title="TAILWINDCSS" />
-            <CardTech iconName="figma" title="FIGMA" />
-            <CardTech iconName="firebase" title="FIREBASE" />
-            <CardTech iconName="java" title="JAVA" />
-            <CardTech iconName="react-native" title="REACT" />
-            <CardTech iconName="trello" title="TRELLO" />
-            <CardTech iconName="git" title="GIT" />
-            <CardTech iconName="github" title="GITHUB" />
-            <CardTech iconName="bootstrap" title="BOOTSTRAP" />
-          </div>{" "}
+        <div className=" md:grid md:grid-cols-2 md:px-10 mt-4">
+          <div>
+            <h2 className=" text-md uppercase mt-0 mb-10 md:text-xl">
+              Some technologies I've worked with:
+            </h2>
+            <div className=" text-gray-500 text-5xl grid grid-cols-3 gap-10 items-center justify-center mb-16 mx-2 px-5 md:text-sm md:grid-cols-5">
+              <CardTech iconName="html" title="HTML" />
+              <CardTech iconName="css" title="CSS" />
+              <CardTech iconName="tailwindcss" title="TAILWINDCSS" />
+              <CardTech iconName="figma" title="FIGMA" />
+              <CardTech iconName="firebase" title="FIREBASE" />
+              <CardTech iconName="java" title="JAVA" />
+              <CardTech iconName="react-native" title="REACT" />
+              <CardTech iconName="trello" title="TRELLO" />
+              <CardTech iconName="git" title="GIT" />
+              <CardTech iconName="github" title="GITHUB" />
+              <CardTech iconName="bootstrap" title="BOOTSTRAP" />
+            </div>{" "}
+          </div>
           <div className="pb-5 flex flex-col items-center">
-            <h2 className=" text-base uppercase font-medium tracking-wider">
+            <h2 className=" text-md uppercase tracking-wider md:text-xl">
               Where I've worked:
             </h2>
             <Link to="https://www3.startupacademy237.com/">
               <img
-                className=""
+                className=" size-44 md:size-52"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUO_z6ZbLsktlQh46D_Ba0CQLmy0nBWVl3p_Orn-eb3H5U8DiRfm5mya_2AjNJ7edYzNQ&usqp=CAU"
                 alt="Startup Academy"
               />
--- src/components/reusable-ui/CardTech.js
+++ src/components/reusable-ui/CardTech.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function CardTech({ iconName, title }) {
   return (
-    <div className="">
+    <div className="flex flex-col items-center justify-center">
       <h4 className="text-[9px]">{title}</h4>
       <img
-        className=" size-20 object-cover contrast-75 grayscale hover:grayscale-0"
+        className=" size-20 object-cover contrast-75 grayscale hover:grayscale-0 md:size-10"
         src={`/assets/icons/icons8-${iconName}-144.png`}
         alt="html"
       />
